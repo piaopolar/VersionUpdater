2666ba171d1f3bd855637cfc19f8ee960f22b68c
piao <piao.polar@gmail.com>
2012-02-15 21:00:39 +0800

    create it

---
+++ VersionUpdater/VersionUpdater.h
@@ -0,0 +1,31 @@
+// VersionUpdater.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CVersionUpdaterApp:
+// See VersionUpdater.cpp for the implementation of this class
+//
+
+class CVersionUpdaterApp : public CWinApp
+{
+public:
+	CVersionUpdaterApp();
+
+// Overrides
+	public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CVersionUpdaterApp theApp;
---
+++ VersionUpdater/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by VersionUpdater.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_VERSIONUPDATER_DIALOG       102
+#define IDR_MAINFRAME                   128
+#define IDC_STA_OLD                     1001
+#define IDC_STA_NEW                     1002
+#define IDC_STA_BEFORE                  1003
+#define IDC_STA_BEFORE2                 1004
+#define IDC_STA_AFTER                   1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
