61203d7a8ba9d5e17f8c881438f45b2a7fa95213
piao <piao.polar@gmail.com>
2012-02-16 17:06:51 +0800

    调整界面

--- VersionUpdater/resource.h
+++ VersionUpdater/resource.h
@@ -10,8 +10,16 @@
 #define IDC_STA_OLD                     1001
 #define IDC_STA_NEW                     1002
 #define IDC_STA_BEFORE                  1003
-#define IDC_STA_BEFORE2                 1004
 #define IDC_STA_AFTER                   1004
+#define IDC_EDIT_OLD                    1005
+#define IDC_EDIT_NEW                    1006
+#define IDC_EDIT_BEFORE                 1007
+#define IDC_EDIT_AFTER                  1008
+#define IDC_CHK_3DMOTION                1009
+#define IDC_CHK_GUI                     1010
+#define IDC_EDIT_LOG                    1011
+#define IDC_BTN_PROCESS                 1012
+#define IDC_BTN_UPDATE                  1012
 
 // Next default values for new objects
 // 
@@ -19,7 +27,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1013
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
